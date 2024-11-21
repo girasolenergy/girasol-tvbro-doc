c036b2dcb3f174c5634b1e86a43f8ea0ba80d517
Kouda (Titanium) <93186844+Kouda-Titanium@users.noreply.github.com>
2024-11-21 15:02:32 +0900

    Update test_plugin_1.kbb.js

--- test_plugin_1.kbb.js
+++ test_plugin_1.kbb.js
@@ -3,7 +3,7 @@
 //   "name": "co.pplc.kanbanbro.plugins.test_plugin_1",
 //   "title": "Test Plugin 1",
 //   "version": "0.0.1",
-//   "description": "example.comの背景色を変えます"
+//   "description": "以下の機能が含まれます◆example.comの背景色を変える◆NHK Pickup NEWSの動画を自動再生"
 // }
 
 if (window.location.href == "https://example.com/") {
@@ -12,5 +12,11 @@ if (window.location.href == "https://example.com/") {
       (Math.floor(Math.random() * 20) + 80) +
       (Math.floor(Math.random() * 20) + 80) +
       (Math.floor(Math.random() * 20) + 80);
+  }, 1000);
+}
+
+if (window.location.href == "https://www3.nhk.or.jp/news/pickup_news16/pickup_news_movie.html") {
+  setTimeout(() => {
+    nPlayerContainerAltContentVideoContentPosterFrame.click();
   }, 1000);
 }
