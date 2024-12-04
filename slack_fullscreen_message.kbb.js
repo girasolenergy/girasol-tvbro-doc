fd8cdc833f4f878877f9ebbb41d8c08aea47907c
Kouda (Titanium) <93186844+Kouda-Titanium@users.noreply.github.com>
2024-12-04 09:44:34 +0900

    Update slack_fullscreen_message.kbb.js

--- slack_fullscreen_message.kbb.js
+++ slack_fullscreen_message.kbb.js
@@ -2,41 +2,52 @@
 // {
 //     "name": "co.pplc.kanbanbro.plugins.slack_fullscreen_message",
 //     "title": "Slack全画面メッセージ",
-//     "version": "1.0.0",
+//     "version": "1.1.0",
 //     "description": "Slackのメッセージ領域を全画面に表示します。"
 // }
 
 if (location.href.startsWith('https://app.slack.com/')) {
-    (function() {
+    let times = 0;
+    const f = function () {
 
-      // メッセージ領域を取得
-      const messagePanes = Array.from(document.querySelectorAll('.p-message_pane'));
-      if (messagePanes.length != 1) {
-          console.warn('[SFM]メッセージ領域を取得できませんでした。');
-          const warningBanner = document.createElement('div');
-          warningBanner.style.position = 'fixed';
-          warningBanner.style.top = '0';
-          warningBanner.style.left = '0';
-          warningBanner.style.width = '100%';
-          warningBanner.style.backgroundColor = 'rgba(255, 128, 0, 0.8)';
-          warningBanner.style.color = 'white';
-          warningBanner.style.padding = '10px';
-          warningBanner.style.zIndex = '1000';
-          warningBanner.style.fontSize = '1.5em';
-          warningBanner.style.textAlign = 'center';
-          warningBanner.textContent = '[SFM]メッセージ領域を取得できませんでした。';
-          document.body.appendChild(warningBanner);
-          return;
-      }
-      const messagePane = messagePanes[0];
+        times++;
+        if (times > 10) {
+            console.warn('[SFM]メッセージ領域を取得できませんでした。');
+            const warningBanner = document.createElement('div');
+            warningBanner.style.position = 'fixed';
+            warningBanner.style.top = '0';
+            warningBanner.style.left = '0';
+            warningBanner.style.width = '100%';
+            warningBanner.style.backgroundColor = 'rgba(255, 128, 0, 0.8)';
+            warningBanner.style.color = 'white';
+            warningBanner.style.padding = '10px';
+            warningBanner.style.zIndex = '1000';
+            warningBanner.style.fontSize = '1.5em';
+            warningBanner.style.textAlign = 'center';
+            warningBanner.textContent = '[SFM]メッセージ領域を取得できませんでした。';
+            document.body.appendChild(warningBanner);
+            setTimeout(() => {
+                warningBanner.parentNode.removeChild(warningBanner);
+            }, 10000);
+            return;
+        }
 
-      // 一旦body配下の全要素を非表示
-      document.querySelectorAll('body > *').forEach(element => {
-          element.style.display = 'none';
-      });
+        // メッセージ領域を取得、できなかったら1秒後に再試行
+        const messagePanes = Array.from(document.querySelectorAll('.p-message_pane'));
+        if (messagePanes.length != 1) {
+            setTimeout(f, 1000);
+            return;
+        }
+        const messagePane = messagePanes[0];
 
-      // メッセージ領域をbody直下に移動
-      document.body.appendChild(messagePane);
+        // 一旦body配下の全要素を非表示
+        document.querySelectorAll('body > *').forEach(element => {
+            element.style.display = 'none';
+        });
 
-    })();
+        // メッセージ領域をbody直下に移動
+        document.body.appendChild(messagePane);
+
+    }
+    setTimeout(f, 0);
 }
