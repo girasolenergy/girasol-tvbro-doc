bfd5f1f15cae9e87305232a31a843398a1055cbf
Kouda (Titanium) <93186844+Kouda-Titanium@users.noreply.github.com>
2024-12-26 07:53:31 +0900

    Update jpex_fullscreen_graph.kbb.js

--- jpex_fullscreen_graph.kbb.js
+++ jpex_fullscreen_graph.kbb.js
@@ -2,33 +2,55 @@
 // {
 //     "name": "co.pplc.kanbanbro.plugins.jpex_fullscreen_graph",
 //     "title": "JPEX全画面グラフ",
-//     "version": "1.0.0",
+//     "version": "1.0.1",
 //     "description": "JPEXのページでグラフを全画面にし、すべての項目を表示します。"
 // }
 
 if (window.location.href == 'https://www.jepx.info/spot_free') {
-  // ページ全体のスクロールバーを消す
-  document.documentElement.style.overflow = 'hidden';
+  function delay(wait) {
+    return new Promise(callback => setTimeout(() => callback(), wait));
+  }
+  function retry(wait, limit, getter) {
+    return new Promise(async callback => {
+      for (let t = 0; t < limit; t++) {
+        if (t > 0) await delay(wait);
+        const result = getter();
+        if (result !== undefined) {
+          callback(result);
+          return;
+        }
+      }
+      callback(undefined);
+    });
+  }
+  new Promise(async () => {
+    // ページ全体のスクロールバーを消す
+    document.documentElement.style.overflow = 'hidden';
 
-  // グラフを全画面にする
-  const chartCard = Array.from(document.querySelectorAll('.card')).find(card => {
-    const header = card.querySelector('.card-header');
-    return header && header.innerText.startsWith('スポット市場日間価格');
-  });
-  chartCard.style.position = 'fixed';
-  chartCard.style.top = '0';
-  chartCard.style.left = '0';
-  chartCard.style.width = '100vw';
-  chartCard.style.height = '100vh';
-  chartCard.style.boxSizing = 'border-box';
-  chartCard.style.zIndex = '99999';
+    // グラフを全画面にする
+    const chartCard = await retry(1000, 10, () => {
+      return Array.from(document.querySelectorAll('.card')).find(card => {
+        const header = card.querySelector('.card-header');
+        return header && header.innerText.startsWith('スポット市場日間価格');
+      });
+    });
+    chartCard.style.position = 'fixed';
+    chartCard.style.top = '0';
+    chartCard.style.left = '0';
+    chartCard.style.width = '100vw';
+    chartCard.style.height = '100vh';
+    chartCard.style.boxSizing = 'border-box';
+    chartCard.style.zIndex = '99999';
 
-  // すべての項目にチェックを入れる
-  const areaCard = Array.from(document.querySelectorAll('.card')).find(card => {
-    const header = card.querySelector('.card-header');
-    return header && header.innerText == '表示したいエリアを選択してください';
-  });
-  Array.from(areaCard.querySelectorAll('.card-body input[type="checkbox"]')).forEach(checkbox => {
-    if (!checkbox.checked) checkbox.click();
-  });
+    // すべての項目にチェックを入れる
+    const areaCard = await retry(1000, 10, () => {
+      return Array.from(document.querySelectorAll('.card')).find(card => {
+        const header = card.querySelector('.card-header');
+        return header && header.innerText == '表示したいエリアを選択してください';
+      });
+    });
+    Array.from(areaCard.querySelectorAll('.card-body input[type="checkbox"]')).forEach(checkbox => {
+      if (!checkbox.checked) checkbox.click();
+    });
+  })
 }
