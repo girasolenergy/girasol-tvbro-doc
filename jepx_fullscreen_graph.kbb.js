75690e2e5341310348d7f9eb5c1df23a310840d9
Kouda (Titanium) <93186844+Kouda-Titanium@users.noreply.github.com>
2025-01-23 15:38:15 +0900

    Update jepx_fullscreen_graph.kbb.js

--- jepx_fullscreen_graph.kbb.js
+++ jepx_fullscreen_graph.kbb.js
@@ -2,7 +2,7 @@
 // {
 //     "name": "co.pplc.kanbanbro.plugins.jepx_fullscreen_graph",
 //     "title": "JEPX全画面グラフ",
-//     "version": "1.0.1",
+//     "version": "1.0.2",
 //     "description": "JEPXのページでグラフを全画面にし、すべての項目を表示します。"
 // }
 
@@ -25,9 +25,12 @@ if (window.location.href == 'https://www.jepx.info/spot_free') {
   }
   new Promise(async () => {
     // ページ全体のスクロールバーを消す
+    console.log("[jepx_fullscreen_graph] hide scrollbar: pre");
     document.documentElement.style.overflow = 'hidden';
+    console.log("[jepx_fullscreen_graph] hide scrollbar: post");
 
     // グラフを全画面にする
+    console.log("[jepx_fullscreen_graph] fullscreen graph: pre");
     const chartCard = await retry(1000, 10, () => {
       return Array.from(document.querySelectorAll('.card')).find(card => {
         const header = card.querySelector('.card-header');
@@ -41,16 +44,20 @@ if (window.location.href == 'https://www.jepx.info/spot_free') {
     chartCard.style.height = '100vh';
     chartCard.style.boxSizing = 'border-box';
     chartCard.style.zIndex = '99999';
+    console.log("[jepx_fullscreen_graph] fullscreen graph: post");
 
     // すべての項目にチェックを入れる
+    console.log("[jepx_fullscreen_graph] check all: pre");
     const areaCard = await retry(1000, 10, () => {
       return Array.from(document.querySelectorAll('.card')).find(card => {
         const header = card.querySelector('.card-header');
         return header && header.innerText == '表示したいエリアを選択してください';
       });
     });
+    console.log("[jepx_fullscreen_graph] check all: areaCard = " + areaCard);
     Array.from(areaCard.querySelectorAll('.card-body input[type="checkbox"]')).forEach(checkbox => {
       if (!checkbox.checked) checkbox.click();
     });
+    console.log("[jepx_fullscreen_graph] check all: post");
   })
 }
