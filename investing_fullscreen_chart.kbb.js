8e8e17459ca7a964dfc06eb1e5c892234db94824
Kouda (Titanium) <93186844+Kouda-Titanium@users.noreply.github.com>
2025-02-05 16:44:58 +0900

    Update investing_fullscreen_chart.kbb.js

--- investing_fullscreen_chart.kbb.js
+++ investing_fullscreen_chart.kbb.js
@@ -2,29 +2,61 @@
 // {
 //     "name": "co.pplc.kanbanbro.plugins.investing_fullscreen_chart",
 //     "title": "Investing全画面グラフ",
-//     "version": "1.0.0",
+//     "version": "1.0.1",
 //     "description": "jp.investing.comでグラフをろうそく足チャートにして全画面表示します。"
 // }
 
 if (window.location.host == 'jp.investing.com') {
-  // ページ全体のスクロールバーを消す
-  document.documentElement.style.overflow = 'hidden';
+  function delay(wait) {
+    return new Promise(callback => setTimeout(() => callback(), wait));
+  }
+  function retry(wait, limit, getter) {
+    return new Promise(async callback => {
+      for (let t = 0; t < limit; t++) {
+        if (t > 0) await delay(wait);
+        const result = await getter();
+        if (result !== undefined) {
+          callback(result);
+          return;
+        }
+      }
+      callback(undefined);
+    });
+  }
+  new Promise(async () => {
 
-  // グラフを全画面にする
-  const container = document.querySelector('.overview-chart .overview-chart-body');
-  container.style.position = 'fixed';
-  container.style.top = '0';
-  container.style.left = '0';
-  container.style.width = '100vw';
-  container.style.height = '100vh';
-  container.style.boxSizing = 'border-box';
-  container.style.zIndex = '99999';
-  container.style.backgroundColor = 'white';
-  container.style.padding = '3vw';
+    // ページ全体のスクロールバーを消す
+    console.log("[investing_fullscreen_chart] removing scroll bar");
+    document.documentElement.style.overflow = 'hidden';
 
-  // UIを消す
-  //container.querySelector('.overview-chart-footer').style.display = 'none';
+    // グラフを全画面にする
+    console.log("[investing_fullscreen_chart] fullscreen");
+    const container = await retry(1000, 10, () => {
+      const container = document.querySelector('.overview-chart .overview-chart-body');
+      return container === null ? undefined : container;
+    });
+    container.style.position = 'fixed';
+    container.style.top = '0';
+    container.style.left = '0';
+    container.style.width = '100vw';
+    container.style.height = '100vh';
+    container.style.boxSizing = 'border-box';
+    container.style.zIndex = '99999';
+    container.style.backgroundColor = 'white';
+    container.style.padding = '3vw';
 
-  // 表示モードをろうそく足チャートにする
-  document.querySelector('button[title="ろうそく足チャート"]').click()
+    // UIを消す
+    //console.log("[investing_fullscreen_chart] removing ui");
+    //container.querySelector('.overview-chart-footer').style.display = 'none';
+
+    // 表示モードをろうそく足チャートにする
+    console.log("[investing_fullscreen_chart] candle");
+    await delay(5000);
+    const button = await retry(1000, 10, () => {
+      const button = document.querySelector('button[title="ろうそく足チャート"]');
+      return button === null ? undefined : button;
+    });
+    button.click()
+
+  });
 }
