f037abbb012e75d6f482cfd12e3c9086e78935aa
Kouda (Titanium) <93186844+Kouda-Titanium@users.noreply.github.com>
2025-04-07 12:33:05 +0900

    Update nhk_pickup_autoplayer.kbb.js

--- nhk_pickup_autoplayer.kbb.js
+++ nhk_pickup_autoplayer.kbb.js
@@ -2,7 +2,7 @@
 // {
 //   "name": "co.pplc.kanbanbro.plugins.nhk_pickup_autoplayer",
 //   "title": "NHK Pickup Autoplayer",
-//   "version": "0.0.3",
+//   "version": "0.0.4",
 //   "description": "NHKのPickup NEWSに以下の機能を追加します。<ul><li>自動再生</li><li>自動ループ</li></ul>"
 // }
 
@@ -34,11 +34,11 @@ if (window.location.href == "https://www3.nhk.or.jp/news/pickup_news16/pickup_ne
     }
     console.log('[nhk_pickup_autoplayer] video: ' + video);
     video.setAttribute('loop', '');
-    await retry(1000, 10, async () => {
+    const result = await retry(1000, 10, async () => {
       const poster = document.getElementById("nPlayerContainerAltContentVideoContentPosterFrame");
       if (poster === null) {
         console.log('[nhk_pickup_autoplayer] poster = null');
-        return null;
+        return false;
       }
       poster.click();
       console.log('[nhk_pickup_autoplayer] clicked');
@@ -47,8 +47,17 @@ if (window.location.href == "https://www3.nhk.or.jp/news/pickup_news16/pickup_ne
         console.log('[nhk_pickup_autoplayer] retry');
         return undefined;
       } else {
-        return null;
+        return true;
       }
     });
+    if (!result) {
+      console.log('[nhk_pickup_autoplayer] failed to play video');
+      return;
+    }
+    while (true) {
+      await delay(10000);
+      const isPlaying = !!(video.currentTime > 0 && !video.paused && !video.ended && video.readyState > 2);
+      if (!isPlaying) video.play();
+    }
   });
 }
