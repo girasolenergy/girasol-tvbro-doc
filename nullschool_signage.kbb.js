6a05fcea98729d116bdef4b74d2c6ad8c3bebf08
Kouda (Titanium) <93186844+Kouda-Titanium@users.noreply.github.com>
2025-04-21 17:04:42 +0900

    Create nullschool_signage.kbb.js

---
+++ nullschool_signage.kbb.js
@@ -0,0 +1,114 @@
+// @KanbanBroPlugin
+// {
+//     "name": "co.pplc.kanbanbro.plugins.nullschool_signage",
+//     "title": "nullschool Signage",
+//     "version": "1.0.0",
+//     "description": "nullschool signage"
+// }
+
+if (location.hostname == "earth.nullschool.net") {
+    new Promise(async () => {
+        function make(e, block) {
+            block(e);
+            return e;
+        }
+        function delay(wait) {
+            return new Promise(resolve => setTimeout(resolve, wait));
+        }
+        async function retry(getter) {
+            let result;
+            for (let i = 0; i < 60; i++) {
+                if (i > 0) await delay(1000);
+                result = getter();
+                if (result !== undefined) return result;
+            }
+            throw new Error("Timeout waiting for element");
+        }
+        function element(query) {
+            return retry(() => {
+                const e = document.querySelectorAll(query);
+                if (e.length == 0) return undefined;
+                if (e.length > 1) throw new Error("Multiple elements found for query: " + query);
+                return e[0];
+            });
+        }
+
+        async function getModeUi() {
+            const buttons = [...(await element('div[data-name="mode"]')).querySelectorAll("button")];
+            const selectedIndex = buttons.findIndex(b => b.getAttribute("aria-selected") == "true");
+            return {
+                buttons: buttons,
+                selectedIndex: selectedIndex,
+                firstButton: buttons[0],
+                currentButton: buttons[selectedIndex],
+                nextButton: buttons[(selectedIndex + 1) % buttons.length]
+            };
+        }
+        async function getOverlayUi() {
+            const buttons = [...(await element('div:not([hidden]) > div[data-name="overlay_type"]')).querySelectorAll("button")];
+            const selectedIndex = buttons.findIndex(b => b.getAttribute("aria-checked") == "true");
+            return {
+                buttons: buttons,
+                selectedIndex: selectedIndex,
+                firstButton: buttons[0],
+                currentButton: buttons[selectedIndex],
+                nextButton: buttons[(selectedIndex + 1) % buttons.length]
+            };
+        }
+        async function updateLabel() {
+            const modeUi = await getModeUi();
+            const overlayUi = await getOverlayUi();
+            label.innerText = modeUi.currentButton.querySelectorAll("span")[0].innerText.trim() + " / " + overlayUi.currentButton.querySelectorAll("span")[0].innerText.trim();
+        }
+
+        // Create label
+        let label = null;
+        document.querySelector("main").appendChild(make(document.createElement("div"), div => {
+            div.classList.add("card");
+            div.innerText = "Loading...";
+            div.style.fontSize = "large";
+            div.style.position = "absolute";
+            div.style.top = "0";
+            div.style.left = "0";
+            div.style.whiteSpace = "nowrap";
+            div.style.margin = "1rem 1rem 0 1rem";
+            label = div;
+        }));
+
+        // Hide UI
+        document.getElementById("location-mark").style.display = "none";
+        document.querySelectorAll(".stack").forEach(e => {
+            e.style.display = "none";
+        });
+        document.querySelectorAll(".cta-bar").forEach(e => {
+            e.style.display = "none";
+        });
+        document.querySelectorAll(".earth-bar").forEach(e => {
+            e.style.display = "none";
+        });
+
+        // Update label
+        updateLabel();
+
+        while (true) {
+
+            await delay(30000);
+
+            // Go to the next mode
+            {
+                const overlayUi = await getOverlayUi();
+                if (overlayUi.selectedIndex == overlayUi.buttons.length - 1) {
+                    const modeUi = await getModeUi()
+                    modeUi.nextButton.click();
+                    const overlayUi2 = await getOverlayUi();
+                    overlayUi2.firstButton.click();
+                    updateLabel();
+                } else {
+                    overlayUi.nextButton.click();
+                    updateLabel();
+                }
+            }
+
+        }
+    });
+}
