b591d7942cee329ad85f1dc217a8ce176b321ce1
Kouda (Titanium) <93186844+Kouda-Titanium@users.noreply.github.com>
2025-04-21 17:34:06 +0900

    Update nullschool_signage.kbb.js

--- nullschool_signage.kbb.js
+++ nullschool_signage.kbb.js
@@ -2,7 +2,7 @@
 // {
 //     "name": "co.pplc.kanbanbro.plugins.nullschool_signage",
 //     "title": "nullschool Signage",
-//     "version": "1.0.0",
+//     "version": "1.0.1",
 //     "description": "nullschool signage"
 // }
 
@@ -61,6 +61,8 @@ if (location.hostname == "earth.nullschool.net") {
             label.innerText = modeUi.currentButton.querySelectorAll("span")[0].innerText.trim() + " / " + overlayUi.currentButton.querySelectorAll("span")[0].innerText.trim();
         }
 
+        conolg.log("[nullschool_signage] Starting nullschool signage plugin");
+
         // Create label
         let label = null;
         document.querySelector("main").appendChild(make(document.createElement("div"), div => {
@@ -74,18 +76,23 @@ if (location.hostname == "earth.nullschool.net") {
             div.style.margin = "1rem 1rem 0 1rem";
             label = div;
         }));
+        conolg.log("[nullschool_signage] Label created");
 
         // Hide UI
         document.getElementById("location-mark").style.display = "none";
+        conolg.log("[nullschool_signage] location-mark hidden");
         document.querySelectorAll(".stack").forEach(e => {
             e.style.display = "none";
         });
+        conolg.log("[nullschool_signage] .stack hidden");
         document.querySelectorAll(".cta-bar").forEach(e => {
             e.style.display = "none";
         });
+        conolg.log("[nullschool_signage] .cta-bar hidden");
         document.querySelectorAll(".earth-bar").forEach(e => {
             e.style.display = "none";
         });
+        conolg.log("[nullschool_signage] .earth-bar hidden");
 
         // Update label
         updateLabel();
@@ -102,11 +109,11 @@ if (location.hostname == "earth.nullschool.net") {
                     modeUi.nextButton.click();
                     const overlayUi2 = await getOverlayUi();
                     overlayUi2.firstButton.click();
-                    updateLabel();
                 } else {
                     overlayUi.nextButton.click();
-                    updateLabel();
                 }
+                updateLabel();
+                conolg.log("[nullschool_signage] Overlay changed");
             }
 
         }
