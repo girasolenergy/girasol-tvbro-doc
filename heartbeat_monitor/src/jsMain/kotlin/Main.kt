b1f67bbaa37b6875e3fddee74036af052644a68e
Titanium <kouda@pplc.co>
2025-10-10 16:10:36 +0900

    Migrate ThemeTogglePlugin to Kotlin

--- heartbeat_monitor/src/jsMain/kotlin/Main.kt
+++ heartbeat_monitor/src/jsMain/kotlin/Main.kt
@@ -3,5 +3,6 @@ package hello
 fun main() {
     Plugins.plugins["FaviconPlugin"] = FaviconPlugin
     Plugins.plugins["TitlePlugin"] = TitlePlugin
+    Plugins.plugins["ThemeTogglePlugin"] = ThemeTogglePlugin
     Plugins.plugins["UpdatePlugin"] = UpdatePlugin
 }
---
+++ heartbeat_monitor/src/jsMain/kotlin/ThemeTogglePlugin.kt
@@ -0,0 +1,27 @@
+package hello
+
+import kotlinx.browser.document
+import kotlinx.browser.window
+
+object ThemeTogglePlugin : Plugin {
+    override fun apply() {
+        UiContainers.topbarRightContainer.prepend(
+            document.createElement("button").also { button ->
+                button.asDynamic().type = "button"
+                KanbanBro.event.addEventListener("themeChanged", { _ ->
+                    button.textContent = when (val theme = KanbanBro.theme) {
+                        null -> "Theme: Auto"
+                        "light" -> "Theme: Light"
+                        "dark" -> "Theme: Dark"
+                        else -> "Theme: $theme"
+                    }
+                })
+                button.addEventListener("click", { _ ->
+                    val oldTheme = KanbanBro.theme
+                    val newTheme = if (oldTheme == null) "light" else if (oldTheme == "light") "dark" else null
+                    window.asDynamic().setTheme(newTheme)
+                })
+            },
+        )
+    }
+}
