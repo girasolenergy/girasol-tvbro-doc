e5175d701993cce20154cc5ee7fec8eb4a6c136e
Titanium <kouda@pplc.co>
2025-10-14 13:13:37 +0900

    Add buildDevelopmentPages Gradle task

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,3 +12,13 @@ val buildPagesTask = tasks.register<Sync>("buildPages") {
     from(sourceSets.named("main").get().resources)
 }
 tasks.named("build").configure { dependsOn(buildPagesTask) }
+
+val buildDevelopmentPagesTask = tasks.register<Sync>("buildDevelopmentPages") {
+    group = "build"
+    val destDir = project.layout.buildDirectory.dir("developmentPages")
+    into(destDir)
+    from(project("heartbeat_monitor").tasks.named("jsBrowserDevelopmentExecutableDistribution")) {
+        into("heartbeat_monitor")
+    }
+    from(sourceSets.named("main").get().resources)
+}
