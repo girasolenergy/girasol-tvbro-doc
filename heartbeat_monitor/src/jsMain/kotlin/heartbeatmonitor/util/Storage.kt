6b2e2631b02ee1ca86fc5faadc13ba92038fe569
Titanium <kouda@pplc.co>
2025-10-16 13:51:02 +0900

    Add Storage utility

---
+++ heartbeat_monitor/src/jsMain/kotlin/heartbeatmonitor/util/Storage.kt
@@ -0,0 +1,23 @@
+package hello.heartbeatmonitor.util
+
+import org.w3c.dom.Storage
+import org.w3c.dom.get
+import org.w3c.dom.set
+
+operator fun Storage.set(key: String, value: String?) {
+    if (value == null) {
+        this.removeItem(key)
+    } else {
+        this[key] = value
+    }
+}
+
+fun Storage.property(key: String): Property<String?> = object : Property<String?> {
+    override fun get(): String? {
+        return this@property[key]
+    }
+
+    override fun set(value: String?) {
+        this@property[key] = value
+    }
+}
