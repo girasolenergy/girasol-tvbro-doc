07e50ab9ec039ec6fc2fa58ee142fe599f547f2b
Titanium <kouda@pplc.co>
2025-10-24 18:25:19 +0900

    Refactor toast event handling to use EventRegistry

--- heartbeat_monitor/src/jsMain/kotlin/heartbeatmonitor/core/Toast.kt
+++ heartbeat_monitor/src/jsMain/kotlin/heartbeatmonitor/core/Toast.kt
@@ -1,13 +1,14 @@
 package heartbeatmonitor.core
 
 import heartbeatmonitor.util.jsObjectOf
-import heartbeatmonitor.util.new
 import kotlinx.browser.document
 import kotlinx.browser.window
-import org.w3c.dom.events.Event
+import mirrg.kotlin.event.EventRegistry
+import mirrg.kotlin.event.emit
+import mirrg.kotlin.event.once
 
 fun showToast(message: String) {
-    val toastEvent = new(window.asDynamic().EventTarget)
+    val onClosed = EventRegistry<Unit, Unit>()
 
     val container = UiContainers.toastContainer
 
@@ -17,17 +18,17 @@ fun showToast(message: String) {
             toastDiv.textContent = message
             window.requestAnimationFrame { toastDiv.classList.add("show") }
 
-            toastEvent.addEventListener("dismiss", {
+            onClosed.once.register {
                 toastDiv.classList.remove("show")
                 toastDiv.addEventListener("transitionend", {
                     container.removeChild(toastDiv)
                 }, jsObjectOf("once" to true))
-            }, jsObjectOf("once" to true))
+            }
 
-            val timer = window.setTimeout({ toastEvent.dispatchEvent(Event("dismiss")) }, 5000)
+            val timer = window.setTimeout({ onClosed.emit() }, 5000)
             toastDiv.addEventListener("click", {
                 window.clearTimeout(timer)
-                toastEvent.dispatchEvent(Event("dismiss"))
+                onClosed.emit()
             })
         },
     )
